e43ad622b71ed8dd289da7c8e3f7f453aed4b7de
Aaron Nassiry <anassiry@salesforce.com>
2021-12-23 08:37:21 -0800

    init

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
-'use strict';
-const fs = require('fs')
-const http = require('http');
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-const requestListener = function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write("Request Method: " + req.method + '<br/>');
+  'use strict';
+  const fs = require('fs')
+  const http = require('http');
+  const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
   const request = new XMLHttpRequest();
   request.open('GET', "http://salesforce.com", false);
   request.send(null);
-  res.write("Date: " + request.getResponseHeader("Date") + '<br/>')
+
+  const requestListener = function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   fs.appendFileSync('./number-of-req.txt', 'z', function (err) {
     if (err) throw err;
   });
   const data = fs.readFileSync('./number-of-req.txt', 'utf-8');
-  const len = (data.length - 1) / 2;
-  res.write("number of requests " + len + '<br/>')
+  const reqNum = data.length 
+  console.log(reqNum)
+
+  if (reqNum == 1){
+    fs.appendFileSync('./weblog.csv', 'Timestamp (UTC),Request Type,Request Number,Source IP' + "\r\n", function (err) {
+    if (err) throw err;
+  });
+
+  }
+  
+  res.write("Date: " + request.getResponseHeader("Date") + '<br/>')
+  fs.appendFileSync('./weblog.csv', request.getResponseHeader("Date") + "\t", function (err) {
+    if (err) throw err;
+  });
+
+  res.write("Request Method: " + req.method + '<br/>');
+  fs.appendFileSync('./weblog.csv', req.method + "\t", function (err) {
+    if (err) throw err;
+  });
+
+  res.write("number of requests " + reqNum + '<br/>')
+  fs.appendFileSync('./weblog.csv', reqNum + "\t", function (err) {
+    if (err) throw err;
+  });
+
   res.write("remote address: " + req.socket.remoteAddress)
+  fs.appendFileSync('./weblog.csv', req.socket.remoteAddress + "\n", function (err) {
+    if (err) throw err;
+  });
+
   res.end()
 }
 
@@ -24,4 +49,7 @@ const server = http.createServer(requestListener);
 server.listen(9999);
 if (fs.existsSync("./number-of-req.txt")) {
   fs.rmSync("./number-of-req.txt")
+}
+if (fs.existsSync("./weblog.csv")) {
+  fs.rmSync("./weblog.csv")
 }
