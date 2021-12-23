c2404fd7a2ec597611bf3befc7d715653eb3bfef
Aaron Nassiry <anassiry@salesforce.com>
2021-12-23 15:40:02 -0800

    finalize docker

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,7 @@ RUN useradd dtm --shell /bin/bash --create-home \
 # Install app dependencies
 # A wildcard is used to ensure both package.json AND package-lock.json are copied
 # where available (npm@5+)
-COPY package*.json ./
+COPY package.json ./
 
 RUN npm install
 # If you are building your code for production
--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 seconds=15
 docker_image="dtm-web-app:latest"
-echo "starting docker image: $docker_image...."
 
+echo "let's build the image..."
+docker build . -t dtm-web-app:latest
+
+echo "starting docker image: $docker_image...."
 docker run -p 9999:9999 $docker_image &
 echo "sleeping for $seconds seconds"
 sleep $seconds
+
 echo "logging into the docker image now...."
 docker run -ti $docker_image bash 
