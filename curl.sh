c10cfa5edc8715ecb56e09af1a45f211a6e93dc7
Aaron Nassiry <anassiry@salesforce.com>
2021-12-23 15:01:13 -0800

    dockerize

---
+++ curl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+node app.js&
+echo "sleeping for 5 seconds..."
+sleep 5
+echo "making 25 calls to this endpoint:  http://localhost:9999"
+for i in {1..25}
+do
+        curl http://localhost:9999
+done
+cat ./weblog.csv
---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+seconds=15
+docker_image="dtm-web-app:latest"
+echo "starting docker image: $docker_image...."
+
+docker run -p 9999:9999 $docker_image &
+echo "sleeping for $seconds seconds"
+sleep $seconds
+echo "logging into the docker image now...."
+docker run -ti $docker_image bash 
