01715e347e04c51439dba8cc42d27aa31b73cd29
aaronnassiry <hmnassiry@gmail.com>
2021-12-26 12:01:06 -0800

    Update query.js

--- bigquery/query.js
+++ bigquery/query.js
@@ -1,13 +1,8 @@
-
 const sqlQuery = `SELECT *
   FROM \`public_b2a8cfd0c03826ed.bucket-b4d65759-3302-dcb0-e913-3016708a967e\``
-
-
 const {BigQuery} = require('@google-cloud/bigquery');
 const bigqueryClient = new BigQuery();
 
-
- 
 const options = {
   keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
   projectId: process.env.GOOGLE_APPLICATION_PROJECT_ID,
