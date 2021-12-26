3fe40e409b3ac1d79e01e19c8788012f66e22ac2
Aaron Nassiry <anassiry@salesforce.com>
2021-12-26 11:58:26 -0800

    bigquery

---
+++ bigquery/bigquery-upload.js
@@ -0,0 +1,18 @@
+const bucketName = 'bucket-b4d65759-3302-dcb0-e913-3016708a967e';
+const filename = '../weblog.csv';
+
+const {Storage} = require('@google-cloud/storage');
+
+const storage = new Storage();
+
+async function uploadFile() {
+  await storage.bucket(bucketName).upload(filename, {
+    gzip: true,
+    metadata: {
+      cacheControl: 'public, max-age=31536000',
+    },
+  });
+
+  console.log(`${filename} uploaded to ${bucketName}.`);
+}
+uploadFile();
