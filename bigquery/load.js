42e3f335fe4362b223546e77f0842dc0e1b83365
Aaron Nassiry <anassiry@salesforce.com>
2021-12-27 10:13:47 -0800

    loadlocalCSVFile

---
+++ bigquery/load.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function main(
+) {
+  const {BigQuery} = require('@google-cloud/bigquery');
+  const loadData = new BigQuery();
+  async function loadLocalCSVFile() {
+    const filename = '../weblog.csv';
+    const datasetId = 'public_b2a8cfd0c03826ed';
+    const tableId = 'bucket-b4d65759-3302-dcb0-e913-3016708a967e';
+    const [job] = await loadData
+      .dataset(datasetId)
+      .table(tableId)
+      .load(filename);
+    console.log(`Job ${job.id} completed.`);
+  }
+  loadLocalCSVFile();
+}
+
+main();
