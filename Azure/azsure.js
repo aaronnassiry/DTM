e3eb310a2d9bc31a5b781ad40bb4ecac1ad77177
aaronnassiry <hmnassiry@gmail.com>
2021-12-27 15:58:50 -0800

    Create azsure.js

---
+++ Azure/azsure.js
@@ -0,0 +1,28 @@
+const Connection = require('tedious').Connection;  
+    const myConfig = {  
+        server: 'interview-sql-server-2.database.windows.net',
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'interview_user', 
+                password: ''  
+            }
+        },
+        options: {
+            encrypt: true,
+            database: 'interview' 
+        }
+    };  
+    const newConnection = new Connection(myConfig);  
+    newConnection.on('connect', function(err) {  
+        console.log("Connected");  
+    });
+    newConnection.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = "CREATE TABLE weblog (Timestamp VARCHAR(255), MethodType VARCHAR(255), RequestNumber VARCHAR(255), SourceIP VARCHAR(255))";
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Table created");
+        });
+      });
